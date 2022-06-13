c2df2da458466d5ca89226af6ab0014e689fd824
oOoRenaudoOo <salliotrenaud@gmail.com>
2022-06-13 16:17:50 -0400

    stripe models migrations

---
+++ database/migrations/2022_06_13_192136_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->boolean('premium');
+            $table->timestamp('starts_at');
+            $table->timestamp('ends_at');
+
+            $table->foreignIdFor(User::class);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
