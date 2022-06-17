1b99c5ec45964c9972a984b0252c5c2c0f793f93
oOoRenaudoOo <salliotrenaud@gmail.com>
2022-06-17 09:08:05 -0400

    stripe - balise script js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,7 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -33,5 +32,7 @@
             </main>
         </div>
         @yield('extra-js')
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
 </html>
